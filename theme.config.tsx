e4d7b12ae132345563c2c775326f3c48ff2c75d5
Heavyrisem <pyo1748@gmail.com>
2023-12-08 02:24:51 +0900

    update theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <span
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyItems: "center",
+        gap: "1rem",
+      }}
+    >
+      <Image
+        src={
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Obsidian_software_logo.svg/1200px-Obsidian_software_logo.svg.png"
+        }
+        width={24}
+        height={24}
+        alt="Obsidian Logo"
+      />
+      Hyunyoung's Obsidian
+    </span>
+  ),
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
-  },
-  chat: {
-    link: "https://discord.com",
+    link: "https://github.com/Heavyrisem/nextra_obsidian_publish",
   },
   docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
-  footer: {
-    text: "Nextra Docs Template",
-  },
 };
 
 export default config;
