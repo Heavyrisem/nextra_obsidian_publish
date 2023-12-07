b8e55c74237d4f6963936f1aec4734b792f77a84
표현영 <pyo1748@estsecurity.com>
2023-12-07 16:08:57 +0900

    setup github pages

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const withNextra = require('nextra')({
 
 /** @type {import('next').NextConfig} */
 module.exports = withNextra({
-  output: 'standalone',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  }
 })
 
 // If you have other Next.js configurations, you can pass them as the parameter:
